90d9f86f42453229beafd5a275c6f6ffa12ab2f6
maxaaboutall <maxaaboutall@gmail.com>
2019-09-12 16:48:04 +0500

    attack
    Attack done

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int defaultSpeed;
+    private int speed;
+    private float dazedTime;
+    public float startDazedTime;
+    
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (dazedTime <= 0)
+        {
+            speed = defaultSpeed;
+        }
+        else
+        {
+            speed = 0;
+            dazedTime -= Time.deltaTime;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        dazedTime = startDazedTime;
+        health -= damage;
+        Debug.Log("Damage TAKEN");
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Transform attackPos;
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+    public LayerMask whatIsEnemies;
+    public int damage;
+    public float attackRadius;
+    
+    void Update()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Collider2D[] enemiesToDamage =
+                    Physics2D.OverlapCircleAll(attackPos.position, attackRadius, whatIsEnemies);
+                foreach (var enemy in enemiesToDamage)
+                {
+                    enemy.GetComponent<Enemy>().TakeDamage(damage);
+                }
+                timeBtwAttack = startTimeBtwAttack;
+            }
+        }
+        else
+            timeBtwAttack -= Time.deltaTime;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(attackPos.position, attackRadius);        
+    }
+}
