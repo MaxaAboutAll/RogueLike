ae789f82690ef2639ca359d025d2ba3abe24543e
Maxim Tretyakov <maxaaboutall@gmail.com>
2020-01-08 15:22:40 +0500

    almost last hands

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -8,6 +8,10 @@ public class HandController : MonoBehaviour
     {
         var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+//        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (Input.mousePosition.x < Screen.width/2)
+            transform.rotation = Quaternion.Euler(new Vector3(0, 180, -angle));
+        else
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
