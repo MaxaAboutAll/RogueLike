b82c0ce4d898d8f38fbe73464d3bd4f234289544
Maxim Tretyakov <maxaaboutall@gmail.com>
2020-01-17 17:19:59 +0500

    gnida

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private int health = 100;
+    [SerializeField] public int health = 100;
     [SerializeField] private int defaultSpeed = 15;
     [SerializeField] private int damage = 20;
     [SerializeField] private float repulsiveForce = 20;
     public int Speed { get; private set; }
     private float dazedTime;
     public float startDazedTime = 1;
+    private bool isDeath = false;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
         if (dazedTime <= 0)
         {
             Speed = defaultSpeed;
+            animator?.SetInteger("CountOfAttack", 0);
         }
         else
         {
@@ -31,8 +40,9 @@ public class Enemy : MonoBehaviour
     {
         dazedTime = startDazedTime;
         health -= damage;
+        StartCoroutine(HasAttacked());
         if (health <= 0)
-            Destroy(gameObject);
+            StartCoroutine(MakeDeath());
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -50,10 +60,31 @@ public class Enemy : MonoBehaviour
     private void Attack(Collider2D other)
     {
         dazedTime = startDazedTime;
+        animator?.Play("SkeletonAttack");
         other.GetComponent<PlayerHealth>().TakeDamage(damage);
         var direction = other.transform.position - transform.position;
         direction.Normalize();
         direction.y = 1;
         other.GetComponent<Rigidbody2D>().AddForce(direction * repulsiveForce, ForceMode2D.Impulse);
+    }
+    private IEnumerator HasAttacked()
+    {
+        animator?.Play("SkeletonHit");
+        yield return new WaitForSeconds(1f);    
+        animator?.Play("SkeletonIdle");
+
+    }
+
+    private IEnumerator MakeDeath()
+    {
+        if (!isDeath)
+        {
+            isDeath = true;
+            animator?.Play("SkeletonDeath");
+            GetComponent<Rigidbody2D>().simulated = false;
+            GetComponent<BoxCollider2D>().size = new Vector2(0.24f, 0.03f);
+            yield return new WaitForSeconds(1f);
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/ZombieEnemy.cs
+++ Assets/Scripts/ZombieEnemy.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ZombieEnemy : MonoBehaviour
@@ -7,11 +6,15 @@ public class ZombieEnemy : MonoBehaviour
     private GameObject player;
     private float speed;
     private Enemy enemyComponent;
+    private Animator animator;
     public float PlayerRange = 3f;
+    private int countOfAttack = 0;
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         enemyComponent = GetComponent<Enemy>();
+        animator = GetComponent<Animator>();
+        animator.SetInteger("Health", enemyComponent.health);
     }
 
     void FixedUpdate()
@@ -22,9 +25,10 @@ public class ZombieEnemy : MonoBehaviour
     private void FlipSprite(bool toLeft)
     {
         if(!toLeft)
-            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-        else
             transform.rotation = Quaternion.Euler(Vector3.zero);
+        else
+            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+
     }
 
     private void MovementLogic()
@@ -34,11 +38,29 @@ public class ZombieEnemy : MonoBehaviour
         if (player && Vector3.Distance(player.transform.position, enemyPos) < PlayerRange)
         {
             var dir = player.transform.position - enemyPos;
+            if (Mathf.Abs(dir.x) < 2.5f)
+            {
+                countOfAttack++;
+                // animator?.SetInteger("CountOfAttack", countOfAttack);
+                // StartCoroutine(HasAttacked());
+            }
+            animator.SetInteger("Speed", (int)dir.x);
             transform.position += new Vector3(dir.normalized.x * speed * Time.deltaTime, 0) ;
             if (dir.x > 0)
                 FlipSprite(false);
             else
                 FlipSprite(true);
         }
+        else
+            animator.SetInteger("Speed", 0);
     }
+    // private IEnumerator HasAttacked()
+    // {
+    //     yield return new WaitForSeconds(0.55f);
+    //     if (countOfAttack > 0)
+    //     {
+    //         countOfAttack = 0;
+    //         animator?.SetInteger("CountOfAttack", countOfAttack);
+    //     }                
+    // }
 }
